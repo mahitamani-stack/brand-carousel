459be2692100c7f83f3e34451fc41e500dec6c1b
mahitamani-stack <mahita.mani@gmail.com>
2026-04-27 16:42:57 +0530

    Adjust position of client heading in Home component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="w-full h-screen bg-black">
             <CarouselScene />
-      <div className="absolute bottom-[28%] left-0 right-0 z-10 text-center pointer-events-none">
+      <div className="absolute bottom-[75%] left-0 right-0 z-10 text-center pointer-events-none">
         <h1 className="font-serif text-5xl font-light text-white">
           Our <em>Clients</em>
         </h1>
