acfcb91d80fa7e59df9544cf8aab0cd9f0487078
mahitamani-stack <mahita.mani@gmail.com>
2026-04-27 17:24:12 +0530

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,14 +1,14 @@
 import { CarouselScene } from "@/components/CarouselScene";
 
 export default function Home() {
-    return (
-          <CarouselScene />
-      <div className="absolute bottom-[28%] left-0 right-0 z-10 text-center pointer-events-none">
-                  <h1 className="font-serif text-2xl font-light text-white">
+  return (
+    <div className="w-full bg-black" style={{ paddingTop: '10px', paddingBottom: '30px' }}>
+      <div className="text-center" style={{ paddingBottom: '8px' }}>
+        <h1 className="font-serif text-2xl font-light text-white">
           Our <em>Clients</em>
         </h1>
       </div>
-        <CarouselScene />
+      <CarouselScene />
     </div>
   );
 }
