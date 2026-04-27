0fe62e84857ba0547cd652884691461465c637c0
mahitamani-stack <mahita.mani@gmail.com>
2026-04-27 16:41:25 +0530

    Remove duplicate CarouselScene from Home component
    Removed duplicate CarouselScene component from Home page.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,12 +3,12 @@ import { CarouselScene } from "@/components/CarouselScene";
 export default function Home() {
   return (
     <div className="w-full h-screen bg-black">
-      <div className="absolute top-0 left-0 right-0 z-10 text-center pt-20 pointer-events-none">
+            <CarouselScene />
+      <div className="absolute bottom-[28%] left-0 right-0 z-10 text-center pointer-events-none">
         <h1 className="font-serif text-5xl font-light text-white">
           Our <em>Clients</em>
         </h1>
       </div>
-      <CarouselScene />
     </div>
   );
 }
