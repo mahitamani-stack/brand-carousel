b8d16b8c4d1a80162a8652a75a40c50803777b58
mahitamani-stack <mahita.mani@gmail.com>
2026-04-27 17:15:47 +0530

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
           <CarouselScene />
       <div className="absolute bottom-[28%] left-0 right-0 z-10 text-center pointer-events-none">
-        <h1 className="font-serif text-5xl font-light text-white">
+                  <h1 className="font-serif text-2xl font-light text-white">
           Our <em>Clients</em>
         </h1>
       </div>
